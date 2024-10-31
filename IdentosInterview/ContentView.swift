2dd9615bd732685b2451d5175373c351b2f862cf
Piraveen Mahesan <12661761+POPTJ47@users.noreply.github.com>
2024-10-30 23:20:22 -0400

    Initial Commit

---
+++ IdentosInterview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IdentosInterview
+//
+//  Created by Piraveen Mahesan on 2024-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IdentosInterview/IdentosInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdentosInterviewApp.swift
+//  IdentosInterview
+//
+//  Created by Piraveen Mahesan on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct IdentosInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
